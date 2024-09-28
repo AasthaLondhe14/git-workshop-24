923fa8c22fa2921443aac25dab012cb928a43b0e
Aastha <l106aasthal.com>
2024-09-28 12:34:43 +0530

    ankitha

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "ankitha",
+    name: "Ankitha ",
+    quote: "you idiot girl",
+  },
 ];
